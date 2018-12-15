e0a4ec2c3c05590abb433fa30d77b1086c5f7ea4
da8865892q <da8865892q@gmail.com>
2018-12-15 23:03:02 +0800

    # Multiply

--- 8kyu/Grasshopper - Summation.py
+++ 8kyu/Grasshopper - Summation.py
@@ -6,4 +6,4 @@ summation = lambda num: sum(x for x in range(num+1))
 
 # best code
 # summation = lambda n: n*(n+1)/2
-# summation = lambda n: sum(range(n + 1))
+# summation = lambda n: sum(range(n+1))
---
+++ 8kyu/Multiply.py
@@ -0,0 +1,8 @@
+# Multiply
+# https://www.codewars.com/kata/multiply/train/python
+
+# My code
+multiply = lambda a, b: a*b
+
+# best code
+# multiply = int.__mul__
