2305120b1bd9644606e771f4d068f35363710635
da8865892q <da8865892q@gmail.com>
2018-12-15 21:00:34 +0800

    grasshopper summation (Issue: PYTHON-10)

---
+++ 8kyu/Grasshopper - Summation.py
@@ -0,0 +1,9 @@
+# Grasshopper - Summation
+# https://www.codewars.com/kata/grasshopper-summation
+
+# My code
+summation = lambda num: sum(x for x in range(num+1))
+
+# best code
+# summation = lambda n: n*(n+1)/2
+# summation = lambda n: sum(range(n + 1))
