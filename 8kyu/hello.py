38acc1bcec5c2637f9450bc7ee1e2cc0df3e13e2
da8865892q <da8865892q@gmail.com>
2018-12-16 19:58:50 +0800

    Hello name or world (Issue: PYTHON-24)

---
+++ 8kyu/hello.py
@@ -0,0 +1,7 @@
+# Hello, Name or World!
+# https://www.codewars.com/kata/hello-name-or-world
+
+# My code
+hello = lambda name: 'Hello, ' + name.capitalize() + '!' if name != "" else 'Hello, World!'
+
+# best code
