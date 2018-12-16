f514717aa40026ecbcbefb11cebb3dfddbc5a53d
da8865892q <da8865892q@gmail.com>
2018-12-16 14:57:31 +0800

    Is the string uppercase (Issue: PYTHON-17)

---
+++ 8kyu/is_uppercase.py
@@ -0,0 +1,8 @@
+# Is the string uppercase?
+# https://www.codewars.com/kata/is-the-string-uppercase
+
+# My code
+is_uppercase = lambda inp: inp.isupper()
+
+# best code
+# is_uppercase = str.isupper
